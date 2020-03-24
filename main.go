d3a129922e084e86e0b0f66966f58d343517260e
jhamPac <cloud@kevia.me>
2020-03-24 06:17:45 -0700

    have to find a better way to complete these

--- main.go
+++ main.go
@@ -1,9 +1,30 @@
 package main
 
-import "flag"
+import (
+	"encoding/csv"
+	"flag"
+	"fmt"
+	"os"
+)
 
 func main() {
 	csvFilename := flag.String("csv", "problems.csv", "A csv file in the format of 'question,answer'")
 	flag.Parse()
-	_ = csvFilename
+	file, err := os.Open(*csvFilename)
+	if err != nil {
+		exit(fmt.Sprintf("Failed to open the CSV file: %s\n", *csvFilename))
+	}
+
+	r := csv.NewReader(file)
+	lines, err := r.ReadAll()
+	if err != nil {
+		exit("Failed to parse the provided CSV file.")
+	}
+
+	_ = lines
+}
+
+func exit(msg string) {
+	fmt.Println(msg)
+	os.Exit(1)
 }
